81f3a841e6fbc8df41a68bc64e2faa39cf91714f
Rafał Babiarz <sauler1995@gmail.com>
2019-12-09 22:11:57 +0100

    First commit

---
+++ Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+
+namespace TestMod
+{
+    public class Main : Mod
+    {
+        public override void Activate()
+        {
+            var harmony = HarmonyInstance.Create("com.sauler.cms.testmod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public override void Deactivate()
+        {
+            
+        }
+
+        public override ModInfo GetInfo()
+        {
+            return new ModInfo
+            {
+                Name = "Test Mod",
+                Description = "Show popup with window name on window show",
+                Author = "Rafał Babiarz",
+                Version = "1.0.0"
+            };
+        }
+    }
+    
+    [HarmonyPatch(typeof(UIManager))]
+    [HarmonyPatch("Show")]
+    [HarmonyPatch(new[] { typeof(string) })]
+    class ShowPopupWithWindowNameOnShow
+    {
+        [HarmonyPrefix]
+        public static void Prefix(UIManager __instance, string windowName)
+        {
+            __instance.ShowPopup("Opened window", windowName, PopupType.Normal);
+        }
+    }
+}
