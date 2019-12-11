b7d4dfc69cc781ed18645b8dc471cb3ab1a94594
Rafał Babiarz <sauler1995@gmail.com>
2019-12-11 18:10:35 +0100

    Updated for new Mod Loader version

--- Main.cs
+++ Main.cs
@@ -1,44 +1,37 @@
 ﻿using System.Reflection;
+using CMS.Mods;
 using Harmony;
 
-namespace ExampleMod
-{
-    public class Main : Mod
-    {
-        private HarmonyInstance _harmonyInstance;
-        public override void Activate()
-        {
-            _harmonyInstance = HarmonyInstance.Create("com.sauler.cms.examplemod");
-            _harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
-            
-        }
+namespace ExampleMod {
+	public class Main : Mod {
+		private HarmonyInstance harmonyInstance;
 
-        public override void Deactivate()
-        {
-            _harmonyInstance.UnpatchAll();
-        }
+		public override void Activate() {
+			harmonyInstance = HarmonyInstance.Create("com.sauler.cms.examplemod");
+			harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+		}
 
-        public override ModInfo GetInfo()
-        {
-            return new ModInfo
-            {
-                Name = "Example Mod",
-                Description = "Show popup with window name on window show",
-                Author = "Rafał Babiarz",
-                Version = "1.0.0"
-            };
-        }
-    }
-    
-    [HarmonyPatch(typeof(UIManager))]
-    [HarmonyPatch("Show")]
-    [HarmonyPatch(new[] { typeof(string) })]
-    class ShowPopupWithWindowNameOnShow
-    {
-        [HarmonyPrefix]
-        public static void Prefix(UIManager __instance, string windowName)
-        {
-            __instance.ShowPopup("Opened window", windowName, PopupType.Normal);
-        }
-    }
+		public override void Deactivate() {
+			harmonyInstance.UnpatchAll();
+		}
+
+		public override ModInfo GetInfo() {
+			return new ModInfo {
+				Name = "Example Mod",
+				Description = "Show popup with window name on window show",
+				Author = "Rafał Babiarz",
+				Version = "1.0.0"
+			};
+		}
+	}
+
+	[HarmonyPatch(typeof(UIManager))]
+	[HarmonyPatch("Show")]
+	[HarmonyPatch(new[] {typeof(string)})]
+	class UIManagerPatch {
+		[HarmonyPrefix]
+		public static void Prefix(UIManager __instance, string windowName) {
+			__instance.ShowPopup("Opened window", windowName, PopupType.Normal);
+		}
+	}
 }
